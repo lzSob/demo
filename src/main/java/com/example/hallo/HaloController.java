9fc36714304560e78d81998351651bea5e3b1d5b
Yukippe <yet.Yukippe@gmail.com>
2018-06-08 16:14:55 +0800

    hello

--- src/main/java/com/example/hallo/HaloController.java
+++ src/main/java/com/example/hallo/HaloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HaloController {
     @RequestMapping(value="/")
     public String say(){
-        return  "Halo Springboot!";
+        return  "Hello Springboot!";
+
     }
 }
