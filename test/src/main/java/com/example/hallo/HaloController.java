2e5d96645c986f2b41315e72c8f86f3bd8c549b6
Yukippe <yet.Yukippe@gmail.com>
2018-06-13 14:07:45 +0800

    test

---
+++ test/src/main/java/com/example/hallo/HaloController.java
@@ -0,0 +1,13 @@
+package com.example.hallo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HaloController {
+    @RequestMapping(value="/")
+    public String say(){
+        return  "Hello Springboot!";
+
+    }
+}
